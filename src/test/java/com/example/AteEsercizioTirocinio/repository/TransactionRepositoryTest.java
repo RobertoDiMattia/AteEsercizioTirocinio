8bb9162dd6a0e4752d5f691bd83ab2926ae9f79a
robertogatsu <robertogatsu@libero.it>
2023-08-08 14:43:38 +0200

    trying JUnit Test implemented application.properties for H2 and its dependencies

--- src/test/java/com/example/AteEsercizioTirocinio/repository/TransactionRepositoryTest.java
+++ src/test/java/com/example/AteEsercizioTirocinio/repository/TransactionRepositoryTest.java
@@ -1,14 +1,13 @@
 package com.example.AteEsercizioTirocinio.repository;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
 class TransactionRepositoryTest {
 
-    @InjectMocks
+    @Autowired
     private TransactionRepository transactionRepository;
 
     @Test
