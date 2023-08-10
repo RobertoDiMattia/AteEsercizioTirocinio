ba930ae70fec4b6818ac0d3780844b763cd00132
robertogatsu <robertogatsu@libero.it>
2023-08-10 21:32:20 +0200

    Updated all JUnit Test Service

--- src/main/java/com/example/AteEsercizioTirocinio/mappers/UserMapper.java
+++ src/main/java/com/example/AteEsercizioTirocinio/mappers/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.AteEsercizioTirocinio.mappers;
 
 import com.example.AteEsercizioTirocinio.dto.UserCreationRequestDto;
+import com.example.AteEsercizioTirocinio.dto.UserDto;
 import com.example.AteEsercizioTirocinio.dto.UserEditDto;
 import com.example.AteEsercizioTirocinio.model.User;
 import org.mapstruct.Mapper;
