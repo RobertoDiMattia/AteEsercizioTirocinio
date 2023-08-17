e1f789df9c7888cccaa9066f93c3842cc1bd1f23
robertogatsu <robertogatsu@libero.it>
2023-08-17 18:21:53 +0200

    JUnit test 5 updated

---
+++ src/test/java/com/example/AteEsercizioTirocinio/utils/TestJacksonConfig.java
@@ -0,0 +1,18 @@
+//package com.example.AteEsercizioTirocinio.utils;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class TestJacksonConfig {
+//
+//    @Bean
+//    public ObjectMapper objectMapper() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.registerModule(new JavaTimeModule());
+//        return objectMapper;
+//    }
+//}
+//
